93a556f9b3bb56ef6c94b79cbb77efe8b101c32e
Claudia Ferguson <clfergus@microsoft.com>
2017-02-25 18:56:02 -0500

    2nd commit

---
+++ stopazurevm.ps1
@@ -0,0 +1,8 @@
+﻿#Stop the VM and Deallocate
+Stop-AzureVM -ServiceName "rangervm001" -Name "rangervm001"
+
+#Stop the VM and do not Deallocate
+Stop-AzureVM -ServiceName "rangervm001" -Name "rangervm001" -StayProvisioned
+ 
+#Stop the VM and Deallocate and no prompt if its the last VM
+Stop-AzureVM -ServiceName "rangervm001" -Name "rangervm001" -Force 
