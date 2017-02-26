c29946508c9f9bac13c2ba16b9153669837419f7
clouditgirl <claudiamcse@hotmail.com>
2017-02-25 19:03:30 -0500

    Delete stopazurevm.ps1
    delete

--- stopazurevm.ps1
+++
@@ -1,8 +0,0 @@
-﻿#Stop the VM and Deallocate
-Stop-AzureVM -ServiceName "rangervm001" -Name "rangervm001"
-
-#Stop the VM and do not Deallocate
-Stop-AzureVM -ServiceName "rangervm001" -Name "rangervm001" -StayProvisioned
- 
-#Stop the VM and Deallocate and no prompt if its the last VM
-Stop-AzureVM -ServiceName "rangervm001" -Name "rangervm001" -Force 
